59db1ff8f7d14089ec24da8e1175846da3593985
rumiani <maziar.rumiani@gmail.com>
2023-04-22 23:08:48 +0100

    task manager added

--- task_manager/mongodb.js
+++ task_manager/mongodb.js
@@ -1,12 +1,25 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-const connectURL = 'mongodb://127.0.0.1:27017/'
-const databaseName = 'nodeTest'
+// const express = require("express");
+// const mongoose = require('mongoose')
+// const app = express()
+
+// const dbURI = 'mongodb+srv://rumi:GxSiV7QXO1EhLC1f@blog.pzttybm.mongodb.net/?retryWrites=true&w=majority'
+// mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => {
+//         app.listen(3000, () => console.log('server is up on port 3000'))
+//         console.log('Connected to db')})
+//         .catch(err => {
+//             console.log('DataBase error: ',err)})
+
+
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+const { MongoClient } = require('mongodb');
+const connectURL = 'mongodb://localhost:27017'
+const databaseName = 'taskManager'
 MongoClient.connect(connectURL, {useNewUrlParser: true}, (err, client)=>{
-    if(err) return console.log("Error: ",err);
-    const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'rumi',
-        age:32
-    })
+    console.log('Testing:');
+    if(err) 
+        return console.log("Error: ",err);
+    // const db = client.db(databaseName)
+    console.log('DB is connected!');
 })
