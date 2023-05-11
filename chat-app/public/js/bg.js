0cb36b98eaaacf2e3703a1779297ba39e7ef6cbb
rumiani <maziar.rumiani@gmail.com>
2023-05-11 10:17:01 +0330

    ui fixing messages corners and centering navbar

--- chat-app/public/js/bg.js
+++ chat-app/public/js/bg.js
@@ -2,7 +2,6 @@ const bg_pic = document.querySelector('#bg_pic')
 const randomBackground = () =>{
     const picNumber = Math.floor(Math.random()*18) +1
     bg_pic.src = `../images/bg/${picNumber}.jpg`;
-    console.log('change');
 }
 randomBackground()
 
