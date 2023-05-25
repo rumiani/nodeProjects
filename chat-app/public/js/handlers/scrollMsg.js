0c9abe4e6c638ffa9e5f90953cc31c4dc0421101
rumiani <maziar.rumiani@gmail.com>
2023-05-25 03:39:58 +0330

    adding scrollDown btn and scroll messages smooth behavidor

--- chat-app/public/js/handlers/scrollMsg.js
+++ chat-app/public/js/handlers/scrollMsg.js
@@ -6,3 +6,21 @@ addEventListener('click', function (e) {
         targetScroll.scrollIntoView({block: "center", behavior: 'smooth' })
     }   
 });
+
+
+const containerEl = document.getElementById('messages_container')
+const scrollDown = document.getElementById('scrollDown')
+const diff = containerEl.scrollHeight - containerEl.scrollTop  
+containerEl.addEventListener('scroll', () =>{    
+    const newDiff = containerEl.scrollHeight - containerEl.scrollTop
+    if (newDiff > diff) {
+        scrollDown.style.display = 'block'
+    }else{
+        scrollDown.style.display = 'none'
+    }
+})
+addEventListener('click', (e) =>{
+        if (e.target.id === 'scrollDown') {
+            containerEl.scrollTop = containerEl.scrollHeight;
+        }
+})
