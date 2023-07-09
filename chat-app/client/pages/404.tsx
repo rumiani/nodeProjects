3020694ff25ea828862e8b837bf93ecd1f3d1aba
rumiani <maziar.rumiani@gmail.com>
2023-07-09 23:59:50 +0330

    redirecting 404

--- chat-app/client/pages/404.tsx
+++ chat-app/client/pages/404.tsx
@@ -5,7 +5,9 @@ const NoutFound = () => {
   return (
     <div className='text-center my-10 mx-auto pt-20 w-80 h-80 shadow-gray-500 shadow-xl rounded-2xl'>
         <h1 className='text-red-600 font-bold'>Page not found.</h1>
-        <Link href='/' className='bg-blue-400 rounded-3xl p-2 hover:text-white hover:bg-blue-600 my-10 mx-auto transition-colors duration-500 block w-40'>Homepage</Link>
+        <Link href='/' className='bg-blue-400 rounded-3xl p-2 hover:text-white hover:bg-blue-600 my-10 mx-auto transition-colors duration-500 block w-40'>
+            Homepage
+        </Link>
     </div>
   )
 }
